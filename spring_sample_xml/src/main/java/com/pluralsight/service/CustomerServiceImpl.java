30fd5f815a6da1c9e3b8f36feb549ce3e2274426
Berchris Requiao <brneto@icloud.com>
2017-09-08 00:53:50 -0300

    Add dependency injection and application context

--- spring_sample_xml/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ spring_sample_xml/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -4,17 +4,28 @@ import java.util.List;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 
 public class CustomerServiceImpl implements CustomerService {
-	private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+	
+	private CustomerRepository customerRepository;
+	
+	public CustomerServiceImpl() {
+		super();
+	}
+	
+	// Used by Spring for Constructor Injection
+	public CustomerServiceImpl(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
 
-	/* (non-Javadoc)
-	 * @see com.pluralsight.service.CustomerService#findAll()
-	 */
 	@Override
 	public List<Customer> findAll() {
 		return customerRepository.findAll();
+	}
+
+	// Used by Spring for Setter Injection
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
 	}
 
 }
