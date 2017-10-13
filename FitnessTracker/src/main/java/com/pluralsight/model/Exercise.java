a3fe1d7a5892b3957aa34716d4ba61e69a2995fe
Berchris Requiao <brneto@icloud.com>
2017-10-12 22:04:25 -0300

    Set Exercise model class to use mysql autoincrement field

--- FitnessTracker/src/main/java/com/pluralsight/model/Exercise.java
+++ FitnessTracker/src/main/java/com/pluralsight/model/Exercise.java
@@ -2,6 +2,7 @@ package com.pluralsight.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
@@ -12,7 +13,7 @@ import org.hibernate.validator.constraints.Range;
 public class Exercise {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 
 	@Range(min = 1, max = 120)
