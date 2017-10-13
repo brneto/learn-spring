3bdd2a55446589f8651d1963cfe499822d51c004
Berchris Requiao <brneto@icloud.com>
2017-10-12 22:05:37 -0300

    Add Exercise save to repository method

--- FitnessTracker/src/main/java/com/pluralsight/service/ExerciseServiceImpl.java
+++ FitnessTracker/src/main/java/com/pluralsight/service/ExerciseServiceImpl.java
@@ -3,12 +3,19 @@ package com.pluralsight.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.pluralsight.model.Activity;
+import com.pluralsight.model.Exercise;
+import com.pluralsight.repository.ExerciseRepository;
 
 @Service("exerciseService")
 public class ExerciseServiceImpl implements ExerciseService {
+	
+	@Autowired
+	private ExerciseRepository exerciseRepository;
 
 	@Override
 	public List<Activity> findAllActivities() {
@@ -28,6 +35,12 @@ public class ExerciseServiceImpl implements ExerciseService {
 		activities.add(swim);
 		
 		return activities;
+	}
+
+	@Override
+	@Transactional
+	public Exercise save(Exercise exercise) {
+		return exerciseRepository.save(exercise);
 	}
 	
 }
