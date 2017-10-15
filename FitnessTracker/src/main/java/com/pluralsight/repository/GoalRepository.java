dffc1e8b296496b91e4e24402abd3a6adfa0f966
Berchris Requiao <brneto@icloud.com>
2017-10-15 18:59:15 -0300

    Remove comment from interface declaration

--- FitnessTracker/src/main/java/com/pluralsight/repository/GoalRepository.java
+++ FitnessTracker/src/main/java/com/pluralsight/repository/GoalRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.pluralsight.model.Goal;
 import com.pluralsight.model.GoalReport;
 
-//By default all method inside of an interface are public.
 public interface GoalRepository {
 	
 	Goal save(Goal goal);
