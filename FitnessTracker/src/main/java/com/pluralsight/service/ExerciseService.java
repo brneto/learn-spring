25eb5f24b06d5e11f3a9ea38de2a3cee2c3fadb6
Berchris Requiao <brneto@icloud.com>
2017-10-15 18:58:52 -0300

    Add comment to interface declaration

--- FitnessTracker/src/main/java/com/pluralsight/service/ExerciseService.java
+++ FitnessTracker/src/main/java/com/pluralsight/service/ExerciseService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.pluralsight.model.Activity;
 import com.pluralsight.model.Exercise;
 
+//By default all method inside of an interface are public.
 public interface ExerciseService {
 
 	List<Activity> findAllActivities();
