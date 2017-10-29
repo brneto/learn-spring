ac699973672355c4225081494658f4af2e7a17b7
Berchris Requiao <brneto@icloud.com>
2017-10-29 01:12:04 -0300

    Update Spring Security to use BCrypt Hash for users password validation.

--- FitnessTracker/src/test/java/PasswordHash.java
+++ FitnessTracker/src/test/java/PasswordHash.java
@@ -1,17 +1,27 @@
 import org.junit.jupiter.api.Test;
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 class PasswordHash {
 
 	@Test
-	void test() {
+	void testBCryptHash() {
+		
+		String password = "secret";
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String hashedPassword = passwordEncoder.encode(password);
+		
+		System.out.println(hashedPassword);
+	}
+	
+	@Test
+	void testMD5Hash() {
 		
 		String password = "secret";
 		Md5PasswordEncoder passwordEncoder = new Md5PasswordEncoder();
 		String hashedPassword = passwordEncoder.encodePassword(password, null);
 		
 		System.out.println(hashedPassword);
-		//fail("Not yet implemented");
 	}
 
 }
