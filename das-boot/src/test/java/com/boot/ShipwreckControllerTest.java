ddc1d0295929cc9e22eb87d9d81463ad9b589c39
Berchris Requiao <berchrisrequiao@mpf.mp.br>
2017-11-07 10:38:04 -0300

    Add controller Hamcrest Matchers

--- das-boot/src/test/java/com/boot/ShipwreckControllerTest.java
+++ das-boot/src/test/java/com/boot/ShipwreckControllerTest.java
@@ -1,15 +1,15 @@
 package com.boot;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import com.boot.controller.ShipwreckController;
 import com.boot.model.Shipwreck;
@@ -37,7 +37,8 @@ public class ShipwreckControllerTest {
 		
 		verify(shipwreckRepository).findOne(1L);
 		
-		assertEquals(1L, wreck.getId().longValue());
+		//assertEquals(1L, wreck.getId().longValue());
+		assertThat(wreck.getId(), equalTo(1L));
 	}
 
 }
