2eb5c00fa9bd1935eedc95d5ec437b31489c6abf
Berchris Requiao <berchrisrequiao@mpf.mp.br>
2017-11-09 08:04:13 -0300

    Add CrossOrigin permission into restful controller

--- das-boot/src/main/java/com/boot/controller/ShipwreckController.java
+++ das-boot/src/main/java/com/boot/controller/ShipwreckController.java
@@ -3,6 +3,7 @@ package com.boot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +36,7 @@ google-chrome --disable-web-security --user-data-dir --allow-file-access-from-fi
 'http://localhost:8080/api/v1/shipwrecks/:id'
 */
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/")
 public class ShipwreckController {
