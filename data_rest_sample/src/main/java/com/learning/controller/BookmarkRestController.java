7380ec0bdd6ff7f4763c2b118ca56a8a8f2a62fb
Berchris Requiao <brneto@icloud.com>
2018-05-27 21:57:45 -0300

    Add file controller (work in progress)

--- data_rest_sample/src/main/java/com/learning/controller/BookmarkRestController.java
+++ data_rest_sample/src/main/java/com/learning/controller/BookmarkRestController.java
@@ -1,42 +1,30 @@
-//package com.learning.controller;
-//
-//import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-//import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
-//
-//import java.net.URI;
-//import java.util.stream.Collectors;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.hateoas.Resource;
-//import org.springframework.hateoas.Resources;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.learning.exception.BookmarkNotFoundException;
-//import com.learning.exception.UserNotFoundException;
-//import com.learning.model.Bookmark;
-//import com.learning.repository.AccountRepository;
-//import com.learning.repository.BookmarkRepository;
-//import com.learning.resource.BookmarkResource;
-//
-//
-//@RestController
-//@RequestMapping("/api")
-//public class BookmarkRestController {
-//	private final BookmarkRepository bookmarkRepository;
-//	private final AccountRepository accountRepository;
-//	
-//	@Autowired
-//	BookmarkRestController(BookmarkRepository bookmarkRepository, AccountRepository accountRepository) {
-//		this.bookmarkRepository = bookmarkRepository;
-//		this.accountRepository = accountRepository;
-//	}
+package com.learning.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.learning.exception.BookmarkNotFoundException;
+import com.learning.repository.BMFileRepository;
+import com.learning.repository.BookmarkRepository;
+
+
+@RepositoryRestController
+@RequestMapping("/api")
+public class BookmarkRestController {
+	private final BookmarkRepository bookmarkRepository;
+	private final BMFileRepository bmfileRepository;
+	
+	@Autowired
+	BookmarkRestController(BookmarkRepository bookmarkRepository, BMFileRepository bmfileRepository) {
+		this.bookmarkRepository = bookmarkRepository;
+		this.bmfileRepository = bmfileRepository;
+	}
 //	
 //	@GetMapping("/{userId}/bookmarks")
 //	public @ResponseBody ResponseEntity<Resources<Bookmark>> readBookmarks(@PathVariable String userId) {
@@ -67,22 +55,33 @@
 //			.orElse(ResponseEntity.noContent().build());
 //	}
 //	
-//	/**
-//	 * Find a single bookmark and transform it into a {@link BookmarkResource}.
-//	 *
-//	 * @param userId
-//	 * @param bookmarkId
-//	 * @return
-//	 */
-//	@GetMapping("/{userId}/bookmarks/{bookmarkId}")
-//	public BookmarkResource readBookmark(@PathVariable String userId, @PathVariable Long bookmarkId) {
-//		this.validateUser(userId);
-//
-//		return this.bookmarkRepository.findById(bookmarkId)
-//			.map(BookmarkResource::new)
-//			.orElseThrow(() -> new BookmarkNotFoundException(bookmarkId));
-//	}
-//
+	/**
+	 * Find a single bookmark and transform it into a {@link BookmarkResource}.
+	 *
+	 * @param userId
+	 * @param bookmarkId
+	 * @return
+	 */
+	@GetMapping("/bookmarks/{bookmarkId}/file")
+	public @ResponseBody ResponseEntity<Resource> readBookmark(@PathVariable Long bookmarkId) {
+		this.validateBookmark(bookmarkId);
+
+		return this.bookmarkRepository.findById(bookmarkId)
+			.map(BMFileRepository::new)
+			.orElseThrow(() -> new BookmarkNotFoundException(bookmarkId));
+	}
+
+	/**
+	 * Verify the {@literal userId} exists.
+	 *
+	 * @param userId
+	 */
+	private void validateBookmark(Long bookmarkId) {
+		this.bookmarkRepository
+			.findById(bookmarkId)
+			.orElseThrow(() -> new BookmarkNotFoundException(bookmarkId));
+	}
+	
 //	/**
 //	 * Verify the {@literal userId} exists.
 //	 *
@@ -93,4 +92,4 @@
 //			.findByUsername(userId)
 //			.orElseThrow(() -> new UserNotFoundException(userId));
 //	}
-//}
+}
