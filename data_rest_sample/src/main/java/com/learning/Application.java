675d1bbfcdcefc7e535c748f158aae509ff0ab1d
Berchris Requiao <brneto@icloud.com>
2018-05-27 23:44:36 -0300

    Remove unused imports

--- data_rest_sample/src/main/java/com/learning/Application.java
+++ data_rest_sample/src/main/java/com/learning/Application.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
